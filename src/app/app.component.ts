a2345ae154036049b28b51d891588b1a2ec4c8db
kevin <kevin.melan00@usc.edu.co>
2021-05-16 10:41:37 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-translate';
+
+  constructor(private translateService: TranslateService) {
+    if (localStorage.getItem('lang')) {
+      this.translateService.setDefaultLang(localStorage.getItem('lang'));
+    } else {
+      localStorage.setItem('lang', 'en');
+      this.translateService.setDefaultLang(localStorage.getItem('lang'));
+    }
+  }
+
+  public useLanguage(language: string): void {
+    this.translateService.use(language);
+    localStorage.setItem('lang', language);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function TranslationLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {provide: TranslateLoader, useFactory: TranslationLoaderFactory, deps: [HttpClient]}
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
